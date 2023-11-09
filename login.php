2169acc12656f9fdaabe12ebb415d4cbd5f10972
0yato <osama1111222@gmail.com>
2023-11-09 09:21:57 +0200

    nothing

---
+++ login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+<?php
+
+if($_POST["userName"].CASE_LOWER=="osama ammar" && $_POST["password"].CASE_LOWER=="oossaammaa1234"){
+    echo "welcome";
+
+}
+else{
+    echo"noo ";
+}
+
+?>
