b830a287437581a2eb2552a48c33941772f52c1f
0yato <osama1111222@gmail.com>
2024-06-06 20:05:02 +0300

    nn

--- index.php
+++ index.php
@@ -33,14 +33,14 @@ session_start();
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Auto </title>
-    <link rel="shortcut icon" type="x-icon" href="image/logo00.svg">
+    <title>Car Shop </title>
+    <link rel="shortcut icon" type="x-icon" href="image/logo0.svg">
 
    
 </head>
 <body>
     <div class="nav">
-        <img src="image/logo00.svg" alt="">
+        <img src="image/logo0.svg" alt="">
     
         <ul>
             <a href="#s"><li>home</li></a>
@@ -77,7 +77,7 @@ session_start();
     <div class="f" id="s">
         <div class="info">
             <div class="te">
-              <p>try to find your dream car with best price with us</p>
+              <p>The perfect car awaits you – explore our collection today!</p>
             </div>
             <a href="login.php">start now</a>
         </div>
@@ -99,13 +99,14 @@ session_start();
 <div class="about" id="about">
     <div class="text">
     <p>
-        <b id="cccc">CarX</b> is a specialized website for displaying cars for rent or sale.
-         It has a large variety of cars from all over the world. Through the website,
-          it makes it easy for you to find the ideal car at the right price and then communicate with
-           the car owner to agree and negotiate via a link that is present in every post.
+        <b id="cccc">CarX</b>Welcome to our premier car marketplace, where your dream car awaits.
+        <br> Browse through our extensive collection of cars from around the world, find the perfect match at the best price, and easily connect with sellers directly through our seamless communication link in every listing.
+        <br> Start your journey with us today and drive home your ideal car!
+
+
     </p>
 </div>
-    <img style="clip-path: polygon(25% 0%, 100% 0%, 100% 100%, 25% 100%, 0% 50%);" src="image/logo2.svg" alt="">
+    <img style="clip-path: polygon(25% 0%, 100% 0%, 100% 100%, 25% 100%, 0% 50%);" src="image/logo0.svg" alt="">
 </div>
 
 
@@ -167,9 +168,9 @@ $_SESSION['signup-state']=false;
 
 
     <div class="card">
-        <h2 class="name">osama ammar</h2>
+        <h2 class="name">Bisan Dweikat</h2>
         
-        <div class="ce">CE | 121</div>
+        <div class="ce">CE | 119</div>
         <div class="line"></div>
         <h2 class="social">social</h2>
         <div class="icons">
@@ -179,7 +180,7 @@ $_SESSION['signup-state']=false;
         </div>
     </div>
     <div class="card">
-        <h2 class="name">Bisan </h2>
+        <h2 class="name">Faiha Odeh </h2>
         
         <div class="ce">CE | 119</div>
         <div class="line"></div>
