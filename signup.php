93207c95bd64b3e96547476778d1cf8aa524ba92
0yato <osama1111222@gmail.com>
2024-06-10 02:57:57 +0300

    BISAN

--- signup.php
+++ signup.php
@@ -22,7 +22,7 @@ $_SESSION['state']='none';
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Car X</title>
+    <title>SPOTLIGHT</title>
 </head>
 <body>
 
